d0f26fa3beeb7ca93b0c08cc82ca61cad647c893
Aswin Krishna <aswinkrishna005@gmail.com>
2025-01-17 10:51:56 +0530

    project setup:->vite,tailwindcss, daisyUI, routing

---
+++ src/components/Body.jsx
@@ -0,0 +1,63 @@
+import Navbar from "./Navbar"
+import Footer from "./Footer"
+import Home from "./Home"
+import About from "./About"
+import Contact from "./Contact"
+import Login from "./Login"
+import Profile from "./Profile"
+import { createBrowserRouter, Outlet, RouterProvider } from "react-router-dom"
+
+const Body = () => {
+    const AppLayout = ()=>{
+        return (
+            <div>
+                <Navbar/>
+                    <Outlet/>
+                <Footer/>
+            </div>
+          )
+    }
+
+    const appRouter = createBrowserRouter([
+        {
+            path:"/",
+            element:<AppLayout/>,
+            children:[
+                {
+                    path:"/",
+                    element:<Home />
+                },
+                {
+                    path:"/login",
+                    element:<Login />
+                },
+                {
+                    path:"/signup",
+                    element:<Login />
+                },
+                {
+                    path:"/about",
+                    element:<About />
+                },
+                {
+                    path:"/contact",
+                    element:<Contact />
+                },
+                {
+                    path:"/profile",
+                    element:<Profile />
+                },
+
+            ]
+        }
+    ])
+
+    return (
+        <div>
+            <RouterProvider router={appRouter}/>
+        </div>
+    )
+ 
+}
+
+export default Body
