9b1f53d9a2f87742ed1399e0bbf0daa5eb1fa9ab
Aswin Krishna <aswinkrishna005@gmail.com>
2025-01-17 21:03:10 +0530

    LOGOUT ( clear the cookies )

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,37 +1,77 @@
-import { USER_ICON } from "../utils/constants"
+import axios from "axios";
+import { useDispatch, useSelector } from "react-redux";
+import { Link, useNavigate } from "react-router-dom";
+import { removeUser } from "../redux/slice/userSlice";
 
 const Navbar = () => {
+  const user = useSelector((store) => store.user);
+  const dispatch = useDispatch()
+  const navigate = useNavigate()
+
+  const handleLogOut = async () => {
+    try {
+       await axios.post(
+        `${import.meta.env.VITE_BACKEND_URL}/auth/logout`,{},
+        { withCredentials: true }
+      );
+      dispatch(removeUser())
+      return navigate("/login")
+
+    } catch (error) {
+      console.error(error);      
+    }
+  };
+
   return (
     <div className="navbar bg-purple-500 rounded-b-2xl">
-  <div className="flex-1">
-    <a className="btn btn-ghost text-white font-extrabold text-2xl">TECHTRIBE</a>
-  </div>
-  <div className="flex-none gap-2 mx-3">
-    
-    <div className="dropdown dropdown-end">
-      <div tabIndex={0} role="button" className="btn btn-ghost btn-circle avatar">
-        <div className="w-10 rounded-full">
-          <img
-            alt="Tailwind CSS Navbar component"
-            src={USER_ICON} />
-        </div>
+      <div className="flex-1">
+        <Link
+          to="/"
+          className="btn btn-ghost text-white font-extrabold text-2xl"
+        >
+          TECHTRIBE
+        </Link>
       </div>
-      <ul
-        tabIndex={0}
-        className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow">
-        <li>
-          <a className="justify-between">
-            Profile
-            <span className="badge">New</span>
-          </a>
-        </li>
-        <li><a>Settings</a></li>
-        <li><a>Logout</a></li>
-      </ul>
+      <div className="flex-none gap-2 mx-3">
+        {user && (
+          <div className="dropdown dropdown-end">
+            <p className="text-white font-semibold tracking-tight">
+              {user.user?.firstName || user.userInfo?.firstName}
+            </p>
+            <div
+              tabIndex={0}
+              role="button"
+              className="btn btn-ghost btn-circle avatar"
+            >
+              <div className="w-10 rounded-full">
+                <img
+                  alt="user-icon"
+                  src={user.user?.photoURL || user.userInfo?.photoURL}
+                />
+              </div>
+            </div>
+            <ul
+              tabIndex={0}
+              className="menu menu-sm dropdown-content bg-base-300 rounded-box z-[1] mt-3 w-52 p-2 shadow"
+            >
+              <li>
+                <Link to="/profile" className="justify-between">
+                  Profile
+                  <span className="badge">New</span>
+                </Link>
+              </li>
+              <li>
+                <Link>Settings</Link>
+              </li>
+              <li>
+                <button onClick={handleLogOut}>Logout</button>
+              </li>
+            </ul>
+          </div>
+        )}
+      </div>
     </div>
-  </div>
-</div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
