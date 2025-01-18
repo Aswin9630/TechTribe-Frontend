b9ecb38f4c9c17a5e1c750a04ef4f9940a45eb27
Aswin Krishna <aswinkrishna005@gmail.com>
2025-01-18 11:21:16 +0530

    feedSlice

---
+++ src/redux/slice/feedSlice.js
@@ -0,0 +1,15 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const feedSlice = createSlice({
+    name:"feed",
+    initialState:null,
+    reducers:{
+        addFeedUsers:(state,action)=> action.payload,
+        removeFeedUsers:(state,action)=> null ,
+        
+    },
+})
+
+
+export const { addFeedUsers, removeFeedUsers } = feedSlice.actions
+export default feedSlice.reducer;
