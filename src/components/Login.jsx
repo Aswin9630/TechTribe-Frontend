0ee2ed5bd0bd806d8f9f29d12078decf46037f3f
Aswin Krishna <aswinkrishna005@gmail.com>
2025-01-18 07:48:27 +0530

    Validation using regex and Error handling

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,8 +1,9 @@
 import axios from "axios";
 import React, { useState } from "react";
-import { useDispatch, useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
 import { addUser } from "../redux/slice/userSlice";
 import { useNavigate } from "react-router-dom";
+import isEmailAndPasswordValid from "../utils/validationRegex";
 
 const Login = () => {
 
@@ -13,8 +14,14 @@ const Login = () => {
   const [password, setPassword] = useState("Aswin@123");
   const [errorMsg, setErrorMsg] = useState(null);
 
+ 
+
   const handleSubmit = async (e) => {
     e.preventDefault();
+    const { EmailError, PasswordError } = isEmailAndPasswordValid(email,password);
+    setErrorMsg({ EmailError, PasswordError });
+    if( EmailError || PasswordError ) return;
+
     try {
       const response = await axios.post(
         `${import.meta.env.VITE_BACKEND_URL}/auth/signin`,
@@ -31,7 +38,7 @@ const Login = () => {
         return navigate("/");
       }
     } catch (error) {
-      setErrorMsg(error.response.data.message)
+      setErrorMsg({serverError:error.response.data.message})
     }
   };
 
@@ -44,6 +51,7 @@ const Login = () => {
             <h1 className="text-white font-bold text-2xl text-center">
               Login!{" "}
             </h1>
+            <div className="flex flex-col gap-5">
             <label className="input input-bordered flex items-center gap-2">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
@@ -56,24 +64,14 @@ const Login = () => {
               </svg>
               <input
                 type="text"
-                className="grow"
+                
                 placeholder="Email"
                 value={email}
                 onChange={(e) => setEmail(e.target.value)}
                 required
               />
             </label>
-            <label className="input input-bordered flex items-center gap-2">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 16 16"
-                fill="currentColor"
-                className="h-4 w-4 opacity-70"
-              >
-                <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6ZM12.735 14c.618 0 1.093-.561.872-1.139a6.002 6.002 0 0 0-11.215 0c-.22.578.254 1.139.872 1.139h9.47Z" />
-              </svg>
-              <input type="text" className="grow" placeholder="Username" />
-            </label>
+              {errorMsg?.EmailError && <p className="text-red-600 text-sm mx-2 font-semibold">{errorMsg.EmailError}</p>}
             <label className="input input-bordered flex items-center gap-2">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
@@ -89,14 +87,19 @@ const Login = () => {
               </svg>
               <input
                 type="password"
-                className="grow"
+                
                 placeholder="Password"
                 value={password}
                 onChange={(e) => setPassword(e.target.value)}
                 required
               />
             </label>
-            <p className="text-red-600 text-sm mx-2 font-semibold">{errorMsg}</p>
+              {errorMsg?.PasswordError && <p className="text-red-600 text-sm mx-2 font-semibold w-56">{errorMsg.PasswordError}</p>}
+            </div>
+           
+          {errorMsg?.serverError &&(
+              <p className="text-red-600 text-sm mx-2 font-semibold">{errorMsg.serverError}</p>
+            )}
             <button
               type="submit"
               className="btn btn-xs text-white font-bold sm:btn-sm md:btn-md lg:btn-md"
