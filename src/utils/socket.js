8921be3bb97eb8ad2145b4ecfd635162ce66c8df
Aswin Krishna <aswinkrishna005@gmail.com>
2025-02-03 20:08:39 +0530

    path setting in socket.js

--- src/utils/socket.js
+++ src/utils/socket.js
@@ -4,11 +4,5 @@ import { BACKEND_URL } from "./constants"
 
 export const createSocketConnection = ()=>{
 
-
-    if(location.hostname === "localhost"){
-
-        return io(BACKEND_URL);
-    }else{
-        return io("wss://techtribe.com/api/socket.io");
-    }
+     return io(BACKEND_URL, { path: "/api/socket.io" }); 
 }
