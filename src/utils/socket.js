e3b0bfbf0f2bf3663d550e834798b03e504fcd71
Aswin Krishna <aswinkrishna005@gmail.com>
2025-02-05 09:56:33 +0530

    .

--- src/utils/socket.js
+++ src/utils/socket.js
@@ -6,9 +6,9 @@ export const createSocketConnection = ()=>{
      if(location.hostname === "localhost"){
           return io(BACKEND_URL)
      }
-     return io("https://thetechtribe.xyz", { path: "/api/socket.io" }),
-
-    { transports: ["websocket", "polling"]} ,
-
-     { withCredentials:true }
+     return io("https://thetechtribe.xyz", { 
+          path: "/api/socket.io",
+          transports: ["websocket", "polling"],
+          withCredentials:true
+     })
 }
