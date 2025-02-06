c7593a00d58aad4a881b8ecbfee41a5884102e5d
Aswin Krishna <aswinkrishna005@gmail.com>
2025-02-06 18:54:11 +0530

    optional chaining

--- src/components/Connection.jsx
+++ src/components/Connection.jsx
@@ -77,13 +77,13 @@ const Connection = () => {
         <h1 className='text-3xl lg:text-4xl font-bold text-center my-7'>Connections</h1>
       <div className='grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 justify-items-center'>        
           {connections.map((user)=>(
-          <div key={user._id} className='w-full max-w-sm flex justify-evenly gap-5 mx-auto items-center border border-yellow-300 m-2 px-2 py-1 rounded-lg'>
+          <div key={user?._id} className='w-full max-w-sm flex justify-evenly gap-5 mx-auto items-center border border-yellow-300 m-2 px-2 py-1 rounded-lg'>
             <div>
               <img src={user?.photoURL} className="w-10 h-10 lg:w-14 lg:h-14 rounded-full" alt="img" />
             </div>
             <div>
-              <h1 className='font-semibold'>{user.firstName} {user.lastName}</h1>
-              <p className='text-sm'>{user.designation}</p>
+              <h1 className='font-semibold'>{user?.firstName} {user?.lastName}</h1>
+              <p className='text-sm'>{user?.designation}</p>
             </div>
              <button onClick={()=>handleChatAccess(user?._id)} className='font-bold tracking-tighter border px-2 py-1 border-yellow-400 hover:bg-gray-900 hover:text-amber-700 rounded-lg'>CHAT</button>
           </div>
