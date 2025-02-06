b11345f612dca21546bbe6281117174076c187c1
Aswin Krishna <aswinkrishna005@gmail.com>
2025-02-06 13:26:15 +0530

    contact

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -3,16 +3,16 @@ import contact_img from "../assets/contact.png"
 
 const Contact = () => {
   return (
-    <div className='flex flex-col lg:flex-row m-2'>
-      <div className='lg:w-1/2 m-3'>
-        <img src={contact_img} alt="contact-image" className='w-96 h-54 mx-auto lg:mx-3 lg:h-auto lg:w-auto'/>
+    <div className='flex flex-col lg:flex-row'>
+      <div className='lg:w-1/2'>
+        <img src={contact_img} alt="contact-image" className='w-96 h-54 mx-auto lg:mx-3'/>
       </div>
 
-        <div className='lg:w-1/2 text-center my-9 text-xl'>
-          <p className='font-bold'>Address :</p>
+        <div className='lg:w-1/2 my-9 text-2xl mx-16'>
+          <p className='font-bold text-amber-700'>Address :</p>
           <p className=''>456 Trendy Lane, <br />Sector 19, Dwarka, <br />New Delhi, Delhi 110328, <br />India</p>
-          <p className='mt-5'>Phone: +91 9876543210 </p>
-          <p>Email: support@thetechtribe.xyz</p>
+          <p className='mt-5'><span className='text-amber-700'>Phone:</span> +91 9876543210 </p>
+          <p><span className='text-amber-700'>Email:</span> support@thetechtribe.xyz</p>
         </div>
     </div>
   )
