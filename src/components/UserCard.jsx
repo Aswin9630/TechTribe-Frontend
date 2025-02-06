efa61c74e8eca186b26f7ea196070f86e9a8ab81
Aswin Krishna <aswinkrishna005@gmail.com>
2025-02-06 13:38:38 +0530

    changing lowercase

--- src/components/UserCard.jsx
+++ src/components/UserCard.jsx
@@ -32,7 +32,7 @@ const UserCard = ({ user, showActions, image }) => {
     { image &&  <div className="w-1/2 mx-auto lg:mx-7">
         <div>
           <h2 className="font-semibold text-2xl lg:text-4xl text-center text-amber-600">Join the Tribe. Explore. Connect. Innovate.
-            <span className="uppercase font-bold text-yellow-500"> —just one click away.
+            <span className="font-bold text-yellow-500"> —just one click away.
           </span></h2>
         </div>
         <img src={images} alt="" className="lg:w-7/12 mx-auto"/>
