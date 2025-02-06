f254f7b648e8a43b0048a683a9d0b27d2e6b13c8
Aswin Krishna <aswinkrishna005@gmail.com>
2025-02-06 13:48:30 +0530

    contact_img

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
-import contact_img from "../assets/contact.png"
+import contact_img from "../assets/contact_us.png"
 
 const Contact = () => {
   return (
-    <div className='flex flex-col lg:flex-row'>
-      <div className='lg:w-1/2'>
-        <img src={contact_img} alt="contact-image" className='w-96 h-54 mx-auto lg:mx-3'/>
+    <div className='flex flex-col lg:flex-row items-center'>
+      <div className='lg:w-1/2 mx-20'>
+        <img src={contact_img} alt="contact-image" className='w-96 h-54 lg:mx-3'/>
       </div>
 
-        <div className='lg:w-1/2 my-9 text-2xl mx-16'>
+        <div className='lg:w-1/2 my-9 text-2xl'>
           <p className='font-bold text-amber-700'>Address :</p>
           <p className=''>456 Trendy Lane, <br />Sector 19, Dwarka, <br />New Delhi, Delhi 110328, <br />India</p>
           <p className='mt-5'><span className='text-amber-700'>Phone:</span> +91 9876543210 </p>
