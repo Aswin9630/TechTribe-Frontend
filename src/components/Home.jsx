bebca169b43b4e52b734194d286d8c56e6e245de
Aswin Krishna <aswinkrishna005@gmail.com>
2025-05-02 19:08:36 +0530

    feed length changes

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -19,7 +19,7 @@ const Home = () => {
   },[feed])
 
   if (loading) return <ShimmerUI />;
-  if (feed.length === 0) {
+  if (feed.length == 0) {
     return <h1 className="text-xl lg:text-3xl font-semibold lg:font-bold my-10 text-center">You reached the daily swipe limit,
      <span className="uppercase text-yellow-700"> Update to Premium </span> 
      for unlimited Swipes and Chat</h1>;
